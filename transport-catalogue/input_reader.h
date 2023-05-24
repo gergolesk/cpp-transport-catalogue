a94caae201ed338a0ff56e65dae3b6651dbf483f
gergolesk <74426622+gergolesk@users.noreply.github.com>
2023-05-24 23:38:09 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,1 +1,15 @@
-// место для вашего кода
+#pragma once
+
+#include "transport_catalogue.h"
+#include <iostream>
+#include <vector>
+
+namespace transport_catalogue {
+
+	namespace input_queries_utils {
+
+		void ParseStopQueries(TransportCatalogue& catalogue, std::vector<std::pair<std::string, std::string>>& stop_queries);
+		void ParseBusQueries(TransportCatalogue& catalogue, std::vector<std::string>& bus_queries);
+		void UpdateTransportCatalogue(TransportCatalogue& catalogue, std::istream& is);
+	}
+}
