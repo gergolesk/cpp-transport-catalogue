6d7329dcc2414f2f2725932e56859c002d8227d4
gergolesk <74426622+gergolesk@users.noreply.github.com>
2023-05-24 23:39:25 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,15 @@
-// место для вашего кода
+#pragma once
+
+#include "transport_catalogue.h"
+#include <iostream>
+#include <vector>
+
+namespace transport_catalogue {
+
+	namespace stat_queries_utils {
+
+		void PrintQueriesData(TransportCatalogue& catalogue, std::vector<std::string>& queries, std::ostream& os);
+		void ReadTransportCatalogue(TransportCatalogue& catalogue, std::istream& is, std::ostream& os);
+
+	}
+}
