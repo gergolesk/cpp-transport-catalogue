322b56cfef406bd9789a24ccc02ed7fcf4d43217
gergolesk <74426622+gergolesk@users.noreply.github.com>
2023-05-24 23:38:31 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,19 @@
-// место для вашего кода
+#include "transport_catalogue.h"
+#include "input_reader.h"
+#include "stat_reader.h"
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    using namespace transport_catalogue;
+
+    TransportCatalogue catalogue;
+    input_queries_utils::UpdateTransportCatalogue(catalogue, cin);
+    stat_queries_utils::ReadTransportCatalogue(catalogue, cin, cout);
+
+    return 0;
+}
